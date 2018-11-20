67e240dab8fc40d91defbe0f620689116e8a3513
Cristina <mailforcris@gmail.com>
2018-11-20 09:23:15 +0100

    routing issue

--- app/assets/config/routes.rb
+++
@@ -1,5 +0,0 @@
-do
-
-endroot 'simple_pages#index'
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -5,3 +5,5 @@ Rails.application.routes.draw do
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+ root :to => 'simple_pages#index'
+ root :to => "products#index"
