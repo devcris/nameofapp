d82755c44c8521fa3e663699b6c2e24249758859
Cristina <mailforcris@gmail.com>
2018-11-20 21:12:14 +0100

    final modification of config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+
 Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
+  get 'simple_pages/', to: 'simple_pages#index'
   root 'simple_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
- root :to => 'simple_pages#index'
- root :to => "products#index"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
