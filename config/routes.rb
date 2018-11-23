86cc4d0ba1cc584c0f280cae5f929e933460c776
Cristina <mailforcris@gmail.com>
2018-11-23 14:42:11 +0100

    added color field to products

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/', to: 'simple_pages#index'
+  get 'simple_pages/index', to: 'simple_pages#index'
   root 'simple_pages#index'
 end
 
