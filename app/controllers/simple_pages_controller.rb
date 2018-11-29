128558777c384a2de8dbf737cd850669369ce18e
Cristina <mailforcris@gmail.com>
2018-11-29 10:53:45 +0100

    4.5 carousel landing page

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,9 +1,10 @@
 class SimplePagesController < ApplicationController
-
-def index
-  @products = Product.limit(3)
-end
   def landing_page
-   @featured_product = Product.limit(3)
-  end
+     @products = Product.limit(3)
+   end
+   def index
+end
+
+def contact
+end
 end
