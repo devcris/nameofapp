bc2394651f7382951aac4ba5bbf657b2cd8378f9
Cristina <mailforcris@gmail.com>
2018-12-05 19:09:20 +0100

    5.3 mailers

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'simple_pages/index', to: 'simple_pages#index'
   get 'simple_pages/landing_page', to: 'simple_pages#landing_page'
   root 'simple_pages#landing_page'
+  post 'simple_pages/thank_you'
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
