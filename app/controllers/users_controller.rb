e5f2774b34c0d7a7f8450e898596d542191d152f
Cristina <mailforcris@gmail.com>
2018-12-06 22:30:15 +0100

    5.5 authorization update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
-  load_and_authorize_resource, except: [:show, :index].
+  load_and_authorize_resource
   # GET /users
   # GET /users.json
   def index
