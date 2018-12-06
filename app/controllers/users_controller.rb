1327e2ef921ce7b889f115bfd8804fb77d38233a
Cristina <mailforcris@gmail.com>
2018-12-06 22:20:54 +0100

    5.5 authorization and alerts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  load_and_authorize_resource
 
   load_and_authorize_resource, except: [:show, :index].
   # GET /users
