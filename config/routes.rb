e6e3f4590448d38488a43844b2d8c41235e6c88d
Cristina <mailforcris@gmail.com>
2018-12-10 00:11:30 +0100

    5.8 comments controles and reviews

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
   get 'simple_pages/landing_page', to: 'simple_pages#landing_page'
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
+
+  resources :products do
+  resources :comments
+end
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
