43fa498ab122286a4f98f715883e94e644758c1c
MMelouk <mmelouk94@gmail.com>
2018-12-12 19:33:42 +0100

    Adjusting spacing and indentations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,18 +1,20 @@
+require 'action_view'
+
 class Comment < ApplicationRecord
-  require 'action_view'
   include ActionView::Helpers::DateHelper
+
   belongs_to :user
   belongs_to :product
-scope :rating_desc, -> { order(rating: :desc) }
-scope :rating_asc, -> { order(rating: :asc) }
-validates :body, presence: true
-validates :user, presence: true
-validates :product, presence: true
-validates :rating, numericality: { only_integer: true }
+  
+  scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
 
+  validates :body, presence: true
+  validates :user, presence: true
+  validates :product, presence: true
+  validates :rating, numericality: { only_integer: true }
 
-def time_ago_in_words
-   distance_of_time_in_words(created_at, Time.now)
-end
-
+  def time_ago_in_words
+    distance_of_time_in_words(created_at, Time.now)
+  end
 end
