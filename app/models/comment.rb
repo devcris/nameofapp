c0466013bd083392cfb5779a28b4dca628fd52fb
Cristina <mailforcris@gmail.com>
2018-12-12 01:16:33 +0100

    updates

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,4 +7,8 @@ validates :body, presence: true
 validates :user, presence: true
 validates :product, presence: true
 validates :rating, numericality: { only_integer: true }
+
+def comment.time_ago_in_words(from_time, options = {})
+  distance_of_time_in_words(from_time, Time.now, options)
+end
 end
