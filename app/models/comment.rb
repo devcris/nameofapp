1878456b9c6b6845a3d0d6b1e9ac1d7e8dfdd4f5
Cristina <mailforcris@gmail.com>
2018-12-13 11:40:20 +0100

    5.9 pagination

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -14,16 +14,6 @@ validates :user, presence: true
 validates :product, presence: true
 validates :rating, numericality: { only_integer: true }
 
-
-  scope :rating_desc, -> { order(rating: :desc) }
-  scope :rating_asc, -> { order(rating: :asc) }
-
-
-  validates :body, presence: true
-  validates :user, presence: true
-  validates :product, presence: true
-  validates :rating, numericality: { only_integer: true }
-
   def time_ago_in_words
     distance_of_time_in_words(created_at, Time.now)
   end
