4b13ff29a8bd93a4e4602e61b5955f3e856f3669
Cristina <mailforcris@gmail.com>
2018-12-13 20:06:03 +0100

    5.11 welcome email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 
 Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }, , controllers: { registrations: "user_registrations" }
   resources :products do
   resources :comments
 end
@@ -14,7 +14,7 @@ end
   get 'simple_pages/landing_page', to: 'simple_pages#landing_page'
   root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
-
+  
 end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
