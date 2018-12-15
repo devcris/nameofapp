76350ff37781ed3a7782a29a7d0e82668e9f7795
Cristina <mailforcris@gmail.com>
2018-12-15 12:57:36 +0100

    5.11 feedback sat

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,4 +1,10 @@
 class UserMailer < ApplicationMailer
+
+  def welcome(user)
+    @appname ="Refurbishes Bikes"
+    mail(to: user. email, 
+       subject:"Welcome to #{@appname}!")
+  end
   def contact_form(email, name, message)
  @message = message
    mail(from: email,
