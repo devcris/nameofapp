6fe83ca76a99a54806c64185cd63443c27482c18
Cristina <mailforcris@gmail.com>
2018-12-17 09:19:59 +0100

    6.3 updates on functional tests

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe UsersController, type: :controller do
     before do
-       @user1 = User.create!(email: 'peter@example.com', password: '1234567890')
-       @user2 = User.create!(email: 'peter@example.com', password: '1234567890')
+       @user1 = User.create!(email: 'peter1@example.com', password: '1234567890')
+       @user2 = User.create!(email: 'peter2@example.com', password: '1234567890')
     end
 
     describe "Get #show" do
