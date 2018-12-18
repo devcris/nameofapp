c4de67fbe93e427a7c660a9e2953f2b435d10f31
Cristina <mailforcris@gmail.com>
2018-12-19 00:52:23 +0100

    6.12 redis gem install

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,6 @@ module Nameofapp
     config.assets.initialize_on_precompile = false
     # Don't generate system test files.
     config.generators.system_tests = nil
-  
+  config.cache_store = :redis_store, 'redis://localhost:6379/0/cache'
   end
 end
