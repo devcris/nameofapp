fd123d1b70959982a1b2e76ddfe0274b29bff3aa
Cristina <mailforcris@gmail.com>
2018-12-18 21:49:04 +0100

    6.11 caching russian doll on products

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,9 +2,9 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 config.action_mailer.default_url_options = { host: 'oldbike.herokuapp.com' }
   # Code is not reloaded between requests.
-  
-  config.cache_classes = true
 
+  config.cache_classes = true
+config.cache_store = :dalli_store
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
