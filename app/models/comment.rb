1c18f684d197d9a30ee30b02239bd86fdf2e6a66
Cristina <mailforcris@gmail.com>
2018-12-20 16:50:45 +0100

    6.14active job advanced

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -9,8 +9,6 @@ after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 scope :rating_desc, -> { order(rating: :desc) }
 scope :rating_asc, -> { order(rating: :asc) }
 
-
-
 validates :body, presence: true
 validates :user, presence: true
 validates :product, presence: true
