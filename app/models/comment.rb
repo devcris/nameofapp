f46e84880ddb066118f7f257d2703e2aee6ccf4c
Cristina <mailforcris@gmail.com>
2018-12-20 11:22:28 +0100

    6.14ActiveJob and Advanced ActionCable

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,9 +5,11 @@ class Comment < ApplicationRecord
  self.per_page = 3
   belongs_to :user
   belongs_to :product
-
+after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 scope :rating_desc, -> { order(rating: :desc) }
 scope :rating_asc, -> { order(rating: :asc) }
+
+
 
 validates :body, presence: true
 validates :user, presence: true
