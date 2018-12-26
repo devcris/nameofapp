49255e3bf5e3694ed87e9fd941873d537a1b7f1a
Cristina <mailforcris@gmail.com>
2018-12-26 22:51:55 +0100

    6.14 shared folder update

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,6 +5,7 @@ class Comment < ApplicationRecord
  self.per_page = 3
   belongs_to :user
   belongs_to :product
+
 after_create_commit { CommentUpdateJob.perform_later(self, self.user) }
 scope :rating_desc, -> { order(rating: :desc) }
 scope :rating_asc, -> { order(rating: :asc) }
