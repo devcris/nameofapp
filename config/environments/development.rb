f575fa50d07f03672410c252dea636ab09fa3ba0
Cristina <mailforcris@gmail.com>
2018-12-29 23:24:27 +0100

    6.14 sign in and last updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,11 +55,12 @@ Rails.application.configure do
 
 #DEACTIVATE DEFAULT COFFEESCRIPT GENERATION
   config.app_generators.javascript_engine = :javascript
-  
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
 end
